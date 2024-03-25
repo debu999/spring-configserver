82db4292696582940cd6575b0cc462e5fac8b674
Debabrata Patnaik <debabrata_patnaik@live.com>
2024-03-25 18:23:16 +0530

    add config server details.

---
+++ src/main/java/org/doogle/springconfigserver/SpringConfigserverApplication.java
@@ -0,0 +1,13 @@
+package org.doogle.springconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigserverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringConfigserverApplication.class, args);
+  }
+
+}
